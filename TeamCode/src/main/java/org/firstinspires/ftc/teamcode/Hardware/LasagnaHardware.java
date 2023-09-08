28426b1e31cb15df3fc5f3b409387c6982ae78a4
Aidan Casey <AidanRCasey06@gmail.com>
2023-09-08 09:49:16 -0500

    Began Team Code
    I began setting up the Hardware, Util, Drive, and Auton Folders.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/LasagnaHardware.java
@@ -0,0 +1,22 @@
+package main.java.org.firstinspires.ftc.teamcode.Hardware;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.robotcore.internal.system.Assert;
+
+public class LasagnaHardware {
+    public DcMotorEx frontLeft = null;
+    public DcMotorEx rearLeft = null;
+    public DcMotorEx frontRight = null;
+    public DcMotorEx rearRight = null;
+
+    public DcMoterEx motors[];
+
+
+public void intializeMotors(HardwareMap hardwareMap){
+    
+}
+}
