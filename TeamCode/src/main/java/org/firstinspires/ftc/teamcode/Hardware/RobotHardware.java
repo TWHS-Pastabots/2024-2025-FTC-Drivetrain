c7aa55393a729cfd9022ea0e837f07e1826103bd
ItsAMeMurio <jdluddeke@gmail.com>
2024-10-29 07:57:59 -0500

    Back to Template
    Someone messed with the template and pushed their own code. Don't do that again pls you have your own repository for a reason :smile_cat: :smile_cat:

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/RobotHardware.java
@@ -2,165 +2,64 @@ package org.firstinspires.ftc.teamcode.Hardware;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.internal.system.Assert;
 
-
-public class RobotHardware
-{
-    // Primary Motors
-    public DcMotorEx leftFront = null;
-    public DcMotorEx leftRear = null;
-    public DcMotorEx rightFront = null;
-    public DcMotorEx rightRear = null;
+public class RobotHardware {
+    public DcMotorEx frontLeft = null;
+    public DcMotorEx rearLeft = null;
+    public DcMotorEx frontRight = null;
+    public DcMotorEx rearRight = null;
 
     public DcMotorEx[] motors;
 
-    // Lift Motors
-    public DcMotorEx liftArm1 = null;
-    public DcMotorEx liftArm2 = null;
+    public RobotHardware() {}
 
-    // CLaw Servo
-    public Servo grabServo = null;
-    public Servo tiltServo = null;
-
-    private PIDFCoefficients PIDF = new PIDFCoefficients(10,0.5,0,45);
-
-    /**
-     * Initializes the drive motors of the robot and
-     * sets them to run without encoder.
-     *
-     * @param hardwareMap   robot's components map
-     */
-    public void initializePrimaryMotors(HardwareMap hardwareMap)
-    {
-        // Primary Motors
-        leftFront = hardwareMap.get(DcMotorEx.class, RobotIDS.LEFT_FRONT_MOTOR);
-        leftRear = hardwareMap.get(DcMotorEx.class, RobotIDS.LEFT_REAR_MOTOR);
-        rightFront = hardwareMap.get(DcMotorEx.class, RobotIDS.RIGHT_FRONT_MOTOR);
-        rightRear = hardwareMap.get(DcMotorEx.class, RobotIDS.RIGHT_REAR_MOTOR);
-
-        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
-        leftRear.setDirection(DcMotorSimple.Direction.REVERSE);
-        rightFront.setDirection(DcMotorSimple.Direction.FORWARD);
-        rightRear.setDirection(DcMotorSimple.Direction.FORWARD);
-
-
-        // Set Zero Power Behavior and Initialize Motors
-        leftRear.setPower(0);
-        leftRear.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
-        leftRear.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        leftFront.setPower(0);
-        leftFront.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
-        leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        rightFront.setPower(0);
-        rightFront.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
-        rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        rightRear.setPower(0);
-        rightRear.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
-        rightRear.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
+    public void init(HardwareMap hardwareMap){
+        Assert.assertNotNull(hardwareMap);
+        initializeDriveMotors(hardwareMap);
+        initializeIntakeMotors(hardwareMap);
+        initializeOutTakeMotors(hardwareMap);
+        initializeServos(hardwareMap);
     }
 
+    public void initializeDriveMotors(HardwareMap hardwareMap){
+        //getting the motor ids from the hardwaremap you will create
+        frontLeft = hardwareMap.get(DcMotorEx.class, RobotIDS.LEFT_FRONT_MOTOR);
+        frontRight = hardwareMap.get(DcMotorEx.class, RobotIDS.RIGHT_FRONT_MOTOR);
+        rearLeft = hardwareMap.get(DcMotorEx.class, RobotIDS.LEFT_REAR_MOTOR);
+        rearRight = hardwareMap.get(DcMotorEx.class, RobotIDS.RIGHT_REAR_MOTOR);
 
 
-    /**
-     * Sets drive motors to run with encoder.
-     */
-    public void turnOnDriveEncoders()
-    {
-        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        leftRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //this is the list of the motors you will use for the drivetrain
+        motors = new DcMotorEx[]{frontLeft, frontRight, rearLeft, rearRight};
 
+        //setting the directions specific to the mecanum drive train. If you have a different drive train come ask.
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        rearLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        rearRight.setDirection(DcMotorSimple.Direction.FORWARD);
+
+
+        for(DcMotorEx motor : motors ){
+            motor.setPower(0.0);
+            motor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+            motor.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        }
     }
 
-
-
-    /**
-     * Initializes the claw servo, sets the position to closed,
-     * and sets the default movement direction.
-     *
-     * @param hardwareMap   robot's components map
-     */
-    public void initializeClawServos(HardwareMap hardwareMap)
-    {
-        // Claw Servo
-        grabServo = hardwareMap.get(Servo.class, RobotIDS.GRAB_SERVO);
-
-        grabServo.setDirection(Servo.Direction.FORWARD);
-        grabServo.setPosition(.6);
-
-        tiltServo = hardwareMap.get(Servo.class, RobotIDS.TILT_SERVO);
-        tiltServo.setDirection(Servo.Direction.FORWARD);
-        tiltServo.setPosition(0.6);
-    }
-    public void initializeClawServosTeleOp(HardwareMap hardwareMap)
-    {
-        // Claw Servo
-        grabServo = hardwareMap.get(Servo.class, RobotIDS.GRAB_SERVO);
-
-        grabServo.setDirection(Servo.Direction.FORWARD);
-        grabServo.setPosition(.33);
-
-        tiltServo = hardwareMap.get(Servo.class, RobotIDS.TILT_SERVO);
-        tiltServo.setDirection(Servo.Direction.FORWARD);
-        tiltServo.setPosition(0.6);
+    public void initializeIntakeMotors(HardwareMap hardwareMap){
+        //this will be where your intake code goes eventually
     }
 
-
-
-    /**
-     * Initializes the lift motors.
-     *
-     * @param hardwareMap   robot's components map
-     */
-    public void initializeSupplementaryMotors(HardwareMap hardwareMap)
-    {
-        // LiftArm2
-        liftArm2 = hardwareMap.get(DcMotorEx.class, RobotIDS.LIFT_ARM2);
-
-        liftArm2.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        liftArm2.setPower(0);
-        liftArm2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        liftArm2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        liftArm2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        // LiftArm1
-        liftArm1 = hardwareMap.get(DcMotorEx.class, RobotIDS.LIFT_ARM1);
-
-        liftArm1.setDirection(DcMotorSimple.Direction.FORWARD);
-
-        liftArm1.setPower(0);
-        liftArm1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        liftArm1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        liftArm1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        liftArm1.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, PIDF);
-        liftArm2.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, PIDF);
+    public void initializeOutTakeMotors(HardwareMap hardwareMap){
+        //this is where your shooter code will go eventually
     }
 
-
-
-    /**
-     * Turns off all motors.
-     */
-    public void robotStopAllMotion() {
-        leftFront.setPower(0);
-        leftRear.setPower(0);
-        rightFront.setPower(0);
-        rightRear.setPower(0);
-
-        liftArm1.setPower(0);
-        liftArm2.setPower(0);
-
+    public void initializeServos(HardwareMap hardwareMap){
+        //this is where all of your servos will go eventually
     }
 }
